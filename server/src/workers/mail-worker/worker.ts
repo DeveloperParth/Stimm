c5ee7d17fc28013c0e1b258be9ffad36b2d1c5ff
Parth <parmarparth996@gmail.com>
2024-12-01 16:12:10 +0530

    refactor mail worker to use constants and improve email sending logic

--- server/src/workers/mail-worker/worker.ts
+++ server/src/workers/mail-worker/worker.ts
@@ -1,14 +1,20 @@
-import { Queue, Worker } from "bullmq";
+import { Worker } from "bullmq";
 import { bullConnection } from "../helper";
 import { sendMailSchema, SendMailSchema } from "../../validation";
-
-const key = "mail";
+import { mailTransport } from "@/lib";
+import { compile } from "@/templates";
+import { mailWorkerConstants } from "./constant";
 
 const worker = new Worker<SendMailSchema>(
-  key,
+  mailWorkerConstants.key,
   async (job) => {
     const data = sendMailSchema.parse(job.data);
-    console.log("Sending mail to", data.to);
+    const html = await compile(data.template, data.context);
+    await mailTransport.sendMail({
+      to: data.to,
+      subject: data.subject,
+      html,
+    });
   },
   {
     connection: bullConnection,
