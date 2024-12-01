9c852ca7f1c63a44e487dc99cbae8bd2dd7addc2
Parth <parmarparth996@gmail.com>
2024-12-01 16:06:50 +0530

    integrate AWS SES for mail transport configuration

--- server/src/lib/index.ts
+++ server/src/lib/index.ts
@@ -1,2 +1,13 @@
 import nodemailer from "nodemailer";
-export const mailTransport = nodemailer.createTransport({});
+import * as aws from "@aws-sdk/client-ses";
+import env from "@/env";
+const ses = new aws.SES({
+  credentials: {
+    accessKeyId: env.aws.ACCESS_KEY,
+    secretAccessKey: env.aws.SECRET_KEY,
+  },
+  region: env.aws.REGION,
+});
+export const mailTransport = nodemailer.createTransport({
+  SES: { ses, aws },
+});
