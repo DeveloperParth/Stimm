146663df5aace07fbef1f1ab0bc0f3c2816db37f
Parth <parmarparth996@gmail.com>
2024-12-01 13:35:13 +0530

    refactor database connection to use pg Pool for improved performance and maintainability

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,15 +1,12 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import env from "../env";
 import * as schema from "./models";
+import { Pool } from "pg";
+const pool = new Pool({
+  user: env.db.USER,
+  host: env.db.HOST,
+  database: env.db.DATABASE,
+  password: env.db.PASSWORD,
+});
 
-export const db = drizzle(
-  {
-    connection: {
-      host: env.db.HOST,
-      user: env.db.USER,
-      password: env.db.PASSWORD,
-      database: env.db.DATABASE,
-    },
-  },
-  { schema }
-);
+export const db = drizzle(pool, { schema });
